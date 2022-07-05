cba423f8356aff5bf872a0b7b60d44e18a1c459b
void <void@null.null>
2022-07-05 11:21:27 +0300

    game info before start

--- Program.cs
+++ Program.cs
@@ -6,6 +6,25 @@ namespace OceanGame
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Press any key to start");
+
+            Console.WriteLine("\nOcean:");
+            Console.WriteLine($"Width:              {GameSettings.OceanWidth}");
+            Console.WriteLine($"Height:             {GameSettings.OceanHeight}");
+            Console.WriteLine($"Cycles:             {GameSettings.GameCycles}");
+
+            Console.WriteLine("\nRatios:");
+            Console.WriteLine($"Obstacle:           {GameSettings.ObstacleRatio}");
+            Console.WriteLine($"Predator:           {GameSettings.PredatorRatio}");
+            Console.WriteLine($"Prey:               {GameSettings.PreyRatio}");
+
+            Console.WriteLine("\nCycles:");
+            Console.WriteLine($"Predator reproduce: {GameSettings.PredatorTimeToReproduce}");
+            Console.WriteLine($"Predator feed:      {GameSettings.PredatorTimeToFeed}");
+            Console.WriteLine($"Prey reproduce:     {GameSettings.PreyTimeToReproduce}");
+
+            Console.ReadKey();
+
             var oceanInterface = new OceanTextInterface(GameSettings.OceanWidth + 10, GameSettings.OceanHeight + 16);
             Ocean ocean = new Ocean(GameSettings.OceanWidth, GameSettings.OceanHeight, oceanInterface);
             ocean.Run(GameSettings.GameCycles);
